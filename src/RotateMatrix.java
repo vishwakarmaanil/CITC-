a5577562640158d323c094ef24e4c8ea6fc3282d
Anil Vishwakarma <vishwakarmaanil321@gmail.com>
2018-04-11 18:39:54 -0500

    as

---
+++ src/RotateMatrix.java
@@ -0,0 +1,17 @@
+
+public class RotateMatrix
+{
+	int[][] rotate(int[][] arr)
+	{
+		return arr;
+	}
+
+	public static void main(String args[])
+	{
+		RotateMatrix r = new RotateMatrix();
+		int arr[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+		System.out.println(arr);
+		r.rotate(arr);
+//		System.out.println(arr);
+	}
+}
